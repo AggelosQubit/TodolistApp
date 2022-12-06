e2d97944b27b4776c629e9bb1a5a7c3c84654d55
AggelosQubit <aggelosqubit@gmail.com>
2022-12-06 16:14:32 +0100

    Last features Delete a Todo UI for Cancelled and Done Todos

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vue 3 + Vite
+# TodoList App in Vue
 
 This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
@@ -7,3 +7,14 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
 
 Todolist Done with the Vue Framework
+
+To test it 
+-Do a pull request ( git pull https://github.com/AggelosQubit/TodolistApp)
+-open the folder via VS code for example
+-Pull up a new terminal inside the folder
+-do a quick npm install
+-Run the command :  npm run dev
+-the web site should be visible at : http://127.0.0.1:5173/
+
+
+Thanks ^^
--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
 		todos.value = JSON.parse(localStorage.getItem('todos') ) || []
 	})
 
-	const addTodo = ()=> {
+	const addTodo = () => {
 		if(input_content.value.trim()=== '' || input_category.value === null ) return 
 
 		todos.value.push( {
@@ -36,6 +36,15 @@
 			createdAt : new Date().getTime()
 		} )
 		console.log("IN ADDTODO");
+
+		input_content.value='';
+		input_category.value=null
+	}
+
+	const removeTodo = (todo) => {
+		todos.value=todos.value.filter( (todoArr) => {return todo !== todoArr });
+		console.table(todos.value)
+		console.log("IN REMOVETODO");
 	}
 </script>
 
@@ -78,17 +87,28 @@
 			<section class="todo-list">
 				<h3>Todo List</h3>
 				<div class="list">
-					<div v-for="todo in todos_asc" :class="`todo-item ${ (todo.done==='Done') && 'done'}`">
-						<label v-if="todo.category==='business'" class=business>
-							{{ todo.category.split("")[0].toUpperCase() +"" + todo.category.substr(1,todo.category.length)  }}
-						</label>
-						<label v-else class="personal">
-							{{ todo.category.split("")[0].toUpperCase() +"" + todo.category.substr(1,todo.category.length)  }}
-						</label>
+					<div v-for="todo in todos_asc" :class="`todo-item ${ (todo.todo_status==='Done' || todo.todo_status==='Cancelled') && 'done'}`">
+						<div class="todo-content">
+							<select v-if="todo.category==='business'" v-model="todo.category" class="business">
+								<option class="business" value="business" selected>Business</option>
+								<option class="personal" value="personal">Personal</option>
+							</select>
+							<select v-else v-model="todo.category" class="personal">
+								<option class="business" value="business" >Business</option>
+								<option class="personal" value="personal" selected>Personal</option>
+							</select>
+						</div>
+						<div class="todo-content">
+							<input type="text" v-model="todo.content" />	
+						</div>
 						<label for="status_Array">Status of the Task : </label>
-						<select v-model="todo.todo_status" >
+						<select class="todo-content" v-model="todo.todo_status" >
 							<option  v-for="status in status_Array" >{{status}}</option>
 						</select>
+
+						<div class="actions ">
+							<button class="delete" @click="removeTodo(todo)" >Delete Task</button>
+						</div>
 					</div> 
 				</div>
 			</section>
